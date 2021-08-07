d9abf3d9b01e1efbc5c78df0ef6b97dd558164c8
And They Have a Plan <etconsilium@github.com>
2021-08-07 16:55:29 +0300

    upgrade

---
+++ pylintrc.py
@@ -0,0 +1,4 @@
+# pylint: disable=R
+# pylint: disable=unused-import               # W0611: Unused import pylintrc (unused-import)
+# pylint: disable=missing-function-docstring  # C0116: Missing function or method docstring (missing-function-docstring)
+# pylint: disable=empty-docstring             # C0112: Empty function docstring (empty-docstring)
