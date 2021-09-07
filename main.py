decc298fb01f3c0aef23e199c45bf4c58e7da6da
And They Have a Plan <etconsilium@github.com>
2021-09-07 14:15:01 +0300

    wip : stash

--- main.py
+++ main.py
@@ -4,16 +4,22 @@
 ##
 #
 """
-module description
+телеграмный бот
+ * автоответчик: сохраняет и показывает сообщения
+ * анонимизатор: в процессе разработки идея отошла на второй план
+
+код иллюстрирует, как не надо писать код
+также содержит примеры вариантов реализации (в комментариях)
+  и хаков, которые описаны в документации, но их применение недостаточно ясно
 """
 
 # pylint:    disable=e0401
 # pylint:    disable=c0112,c0116
 
-
 # import time
 # import requests
 import os
+import sys
 import json
 
 from hashlib import sha1
@@ -31,83 +37,113 @@ from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
+
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Depends
 from fastapi import Request
 from fastapi import Response
 from fastapi import status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import HTMLResponse
 from fastapi.responses import JSONResponse
-from fastapi.responses import RedirectResponse
 
-load_dotenv('.env')
-config = dotenv_values('.env')
+# from fastapi.requests import Session  #   notfound
+from requests import Session
 
-FILE_TOKEN = WEBHOOK_DOMAIN = WEBHOOK_PATH = WEBHOOK_URL = None
+from deta import Deta
+
+# from deta import FetchResponse    #    notfound
 
 
-TG_TOKEN = os.getenv('TG_TOKEN', "#_#")
-TG_MAX_CONNECTION = int(os.getenv('TG_MAX_CONNECTION', 40))
-TG_DROP_UPDATES = bool(os.getenv('TG_DROP_UPDATES', None))
-TG_TIMEOUT = int(os.getenv('TG_TIMEOUT', 10))
-TG_PARSE_MODE = os.getenv('TG_PARSE_MODE', None)
+FILE_TOKEN = WEBHOOK_DOMAIN = WEBHOOK_PATH = WH_URL = None
 
-# https://habr.com/ru/company/ods/blog/462141/
-app = FastAPI()
-# bot = telebot.TeleBot(TG_TOKEN, threaded=False, parse_mode=TG_PARSE_MODE)
-bot = telebot.TeleBot(TG_TOKEN, parse_mode=TG_PARSE_MODE)
+load_dotenv(".env")
+# config = dotenv_values(".env")
+
+
+import commands as command
+from commands import *
 
 
 def file_hash(filename=__file__):
-    '''utils'''
-    return sha1(str(os.stat(filename)).encode('utf8')).hexdigest()
+    """utils"""
+    return sha1(str(os.stat(filename)).encode("utf8")).hexdigest()
 
 
 def wh_url():
-    '''utils'''
-    # return '/bot'
+    """utils"""
     return (
-        WEBHOOK_DOMAIN + (f":{WEBHOOK_PORT}" if bool(WEBHOOK_PORT) else '') + wh_path()
+        WEBHOOK_DOMAIN + (f":{WEBHOOK_PORT}" if bool(WEBHOOK_PORT) else "") + wh_path()
     )
 
 
 def wh_path():
-    '''utils'''
+    """utils"""
+    return "/bot"
     return (
         WEBHOOK_PATH
-        + '/'
-        + sha1(f"{FILE_TOKEN}:{TG_TOKEN}".encode('utf8')).hexdigest()
-        + '/'
+        + "/"
+        + sha1(f"{FILE_TOKEN}:{TG_TOKEN}".encode("utf8")).hexdigest()
+        + "/"
     )
 
 
 def set_webhook():
-    global FILE_TOKEN, WEBHOOK_DOMAIN, WH_URL
+    # A JSON-serialized list of the update types you want your bot to receive. For example, specify [“message”, “edited_channel_post”, “callback_query”] to only receive updates of these types. See Update for a complete list of available update types. Specify an empty list to receive all update types except chat_member (default). If not specified, the previous setting will be used.
+    # https://core.telegram.org/bots/api#getting-updates
+    ALLOWED_UPDATES = [
+        "message",
+        "edited_message",
+        "channel_post",
+        "edited_channel_post",
+        "inline_query",
+        "chosen_inline_result",
+        "callback_query",
+        "shipping_query",
+        "poll",
+        "poll_answer",
+        "my_chat_member",
+        "chat_member",
+    ]
 
+    # var_dump('set_webhook')
+
+    global FILE_TOKEN, WEBHOOK_DOMAIN, WH_URL
+    bot = command.bot
+
+    # https://api.telegram.org/bot{TOKEN}/getWebhookInfo
     bot_nfo = bot.get_webhook_info()
-    # {
-    #     "ok": true,
-    #     "result": {
-    #         "url": "",
-    #         "has_custom_certificate": false,
-    #         "pending_update_count": 9,
-    #     },
-    # }
-    if WH_URL != bot_nfo.url:
-        FILE_TOKEN = file_hash()
-        WH_URL = wh_url()
+    fh = file_hash()
+    wh = wh_url()
+
+    # var_dump(fh, wh, not (WH_URL != bot_nfo.url and FILE_TOKEN != fh))
+
+    if not (WH_URL == bot_nfo.url and FILE_TOKEN == fh):
+        FILE_TOKEN = fh
+        WH_URL = wh
 
         bot.remove_webhook()
-        # def set_webhook(token, url=None, certificate=None, max_connections=None, allowed_updates=None, ip_address=None,
-        #             drop_pending_updates = None, timeout=None):
+
+        #         set_webhook(
+        #             token,
+        #             url=None,
+        #             certificate=None,
+        #             max_connections=None,
+        #             allowed_updates=None,
+        #             ip_address=None,
+        #             drop_pending_updates=None,
+        #             timeout=None,
+        #         ):
+
         bot.set_webhook(
             url=WH_URL,
             max_connections=TG_MAX_CONNECTION,
-            timeout=TG_TIMEOUT,
             allowed_updates=[],
             # allowed_updates=ALLOWED_UPDATES,
-            drop_pending_updates=TG_DROP_UPDATES,
+            # drop_pending_updates=TG_DROP_UPDATES,
+            drop_pending_updates=True,
+            timeout=TG_TIMEOUT,
         )
 
     log.debug(bot.get_webhook_info())
@@ -115,65 +151,80 @@ def set_webhook():
     return
 
 
+FILE_TOKEN = file_hash()
+WEBHOOK_DOMAIN = os.getenv("WEBHOOK_DOMAIN", "")
+WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "/bot")
+WEBHOOK_PORT = os.getenv("WEBHOOK_PORT", "")
+WH_PATH = wh_path()
+WH_URL = wh_url()
+
+
+# Initialize with a Project Key
+TG_TOKEN = os.getenv("TG_TOKEN", "#_#")
+TG_MAX_CONNECTION = int(os.getenv("TG_MAX_CONNECTION", 40))
+TG_DROP_UPDATES = bool(os.getenv("TG_DROP_UPDATES", None))
+TG_TIMEOUT = int(os.getenv("TG_TIMEOUT", 10))
+TG_PARSE_MODE = os.getenv("TG_PARSE_MODE", None)
+
+deta = Deta(os.getenv("DETA_PROJECT_KEY"))
+DETA_DB_NAME = os.getenv("DETA_DB_NAME", TG_TOKEN)
+DB = deta.Base(DETA_DB_NAME)
+SESSION_DB = deta.Base("SESSION")
+
+
+# https://habr.com/ru/company/ods/blog/462141/
+app = FastAPI()
+
+
+# import utils as util
+# from utils import *
+
 ####
 ##
 # Begin
 #
 
-
-# A JSON-serialized list of the update types you want your bot to receive. For example, specify [“message”, “edited_channel_post”, “callback_query”] to only receive updates of these types. See Update for a complete list of available update types. Specify an empty list to receive all update types except chat_member (default). If not specified, the previous setting will be used.
-# https://core.telegram.org/bots/api#getting-updates
-ALLOWED_UPDATES = [
-    'message',
-    'edited_message',
-    'channel_post',
-    'edited_channel_post',
-    'inline_query',
-    'chosen_inline_result',
-    'callback_query',
-    'shipping_query',
-    'poll',
-    'poll_answer',
-    'my_chat_member',
-    'chat_member',
-]
-
-
-FILE_TOKEN = file_hash()
-WEBHOOK_DOMAIN = os.getenv('WEBHOOK_DOMAIN', '')
-WEBHOOK_PATH = os.getenv('WEBHOOK_PATH', '/bot')
-WEBHOOK_PORT = os.getenv('WEBHOOK_PORT', '')
-WH_PATH = wh_path()
-WH_URL = wh_url()
-
-
-if os.getenv('TG_MODE') == 'updates':
+if os.getenv("TG_MODE") == "updates":
     bot.polling(none_stop=True, timeout=TG_TIMEOUT)
+    # bot = telebot.TeleBot(TG_TOKEN, threaded=False, parse_mode=TG_PARSE_MODE)
+    # bot = telebot.TeleBot(TG_TOKEN, parse_mode=TG_PARSE_MODE)
 else:
     set_webhook()
-
-BEEP_STATUS = None
 
 
 # pylint: disable=unused-argument
 @app.post(WH_PATH, response_class=Response, status_code=200)
-async def webhook(request: Request, response: Response, payload: dict = Body(...)):
-    '''process only requests with correct bot token'''
+async def webhook(
+    request: Request,
+    response: Response,
+    # db: Session = Depends(get_db),    # get_db: interface yield
+    payload: dict = Body(...),
+):
+    """process only requests with correct bot token"""
 
     # payload = json.loads( await request.body() )
+    # message = payload['message']
 
-    # if (not bool(payload)) and hasattr(payload, 'update_id'):
-    if 'update_id' in payload:
+    # var_dump('update_id' in payload)
+    if "update_id" in payload:
         #   copypaste from bot.get_updates()
         data = [types.Update.de_json(ju) for ju in [payload]]
+        # data - Object of type Update is not JSON serializable
         bot.process_new_updates(data)
+        var_dump("bot.process_new_updates")
 
     else:
-        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
-        # return JSONResponse(
-        #     status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        #     content={"detail": jsonable_encoder(payload)},
-        # )
+        if "cron" in payload:
+            response.status_code = status.HTTP_203_NON_AUTHORITATIVE_INFORMATION
+            #             bot.process_new_updates([])
+            bot.process_new_messages([])
+        else:
+            response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    # return JSONResponse(
+    #     status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    #     content={"detail": jsonable_encoder(payload)},
+    # )
 
     return
 
@@ -184,156 +235,29 @@ async def webhook(request: Request, response: Response, payload: dict = Body(...
 @app.get("/{param}/", response_class=HTMLResponse, status_code=403)
 async def docroot(param=""):  # pylint: disable=unused-argument
     """ default route """
+
     set_webhook()
 
-    global FILE_TOKEN
-
     return f"And They Have a Plan : {FILE_TOKEN}"
-
-
-@bot.message_handler(commands=["start"])
-def start_command(message: types.Update):
-    print("start here")
-
-    t = message.entities[0]
-    if len(message.text) > t.length:
-        # bot.send_message(message.chat.id, 'start abonent box')
-        BEEP_STATUS = 1
-        start_beep(message)
-    else:
-        # bot.send_message(message.chat.id, 'start text')
-        welcome_message(message)
-    return
-
-
-@bot.message_handler(commands=["help"])
-def help_command(message: types.Update):
-    text = """
-/beep - [секретик] : записать приветственное сообщение
-/start - секретик : отправить сообщение абоненту 
-/beep - beep (два раза) : прочитать сообщения
-/check - проверить сообщения (как два бип)
-/source - sourcecode
-/help - список команд
-"""
-    bot.send_message(message.chat.id, text)
-    # bot.reply_to(message, message.text)
-    return
-
-
-@bot.message_handler(commands=["beep"])
-def beep_command(message: types.Update):
-
-    beep = 'beep'
-
-    return
-
-
-@bot.message_handler(commands=["check"])
-def check_command(message: types.Update):
-
-    echo_message(message)
-
-    return
-
-
-@bot.message_handler(commands=["source"])
-def source_command(message: types.Update):
-    # bot.reply_to(message, '<pre>' + open(__file__, "rb") + '</pre>')
-
-    from pathlib import Path
-
-    MAX_LENGHT = 4096
-    # MAX_LENGHT = 8192
-    # r = '<pre>' + Path(__file__).read_text() + '</pre>'
-    r = Path(__file__).read_text()
-    if len(r) > MAX_LENGHT:
-        for x in range(0, len(r), MAX_LENGHT):
-            bot.send_message(
-                message.chat.id,
-                '<pre><code>{}</code></pre>'.format(r[x : x + MAX_LENGHT]),
-                parse_mode=None,
-            )
-
-    return
-
-
-@bot.message_handler(commands=["beep"])
-def start_beep(message: types.Update):
-
-    global BEEP_STATUS
-    beep = 'Beep'
-
-    t = message.entities[0]
-
-    if len(message.text) > t.length:
-
-        BEEP_STATUS = 2 if 1 == BEEP_STATUS else 1
-
-        abonent_box = (message.text[t.offset + t.length :]).strip()
-
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-    buttonB = types.KeyboardButton(f'/{beep}!')
-    markup.row(buttonB)
-
-    # InlineKeyboardButton(text, url=None, callback_data=None, switch_inline_query=None, switch_inline_query_current_chat=None, callback_game=None, pay=None, login_url=None, **_kwargs)
-    # markup = types.InlineKeyboardMarkup()
-    # buttonB = types.InlineKeyboardButton('Beep!')
-    # markup.row(buttonB)
-
-    # user_id = message.from.id
-    chat_id = message.chat.id
-
-    """
-    allowed html tags
-    <b>bold</b>, <strong>bold</strong> <i>italic</i>, <em>italic</em> <a href="URL">inline URL</a>
-    <code>inline fixed-width code</code> <pre>pre-formatted fixed-width code block</pre>
-    """
-    text = (
-        f"Hi there, You have reached the postponement machine with a combination <a href='https://t.me/post_after_bot?start={abonent_box}'><b>{abonent_box.upper()}</b></a>. "
-        + f" Post a message after the {beep}!"
-        + os.linesep * 2
-        + f"Нажмите кнопку и напишите сообщение для абонента с кодовым именем <a href='https://t.me/post_after_bot?start={abonent_box}'><b>{abonent_box.upper()}</b></a>"
-    )
-    bot.send_message(message.chat.id, text, reply_markup=markup)
-
-    return
-
-
-@bot.message_handler(content_types=["text"])
-def echo_message(message: types.Update):
-    # bot.reply_to(message, message.text)
-    bot.send_message(message.chat.id, message.text)
-    return
-
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def welcome_message(message: types.Update):
-    # bot.reply_to(message, message.text)
-    # bot.send_message(message.chat.id, message.text)
-    bot.send_message(message.chat.id, 'welcome')
-    return
 
 
 def main():
     """Start here"""
     log.info("Start main()")
+    log.debug(bot)
+
     import uvicorn
 
     uvicorn.run(app)
 
-    if os.getenv('TG_MODE') == 'webhook':
+    if os.getenv("TG_MODE") == "webhook":
         set_webhook()
     else:
         bot.polling(none_stop=True, timeout=TG_TIMEOUT)
 
-    exit("Bot exited")
+    sys.exit("Bot exited")
 
 
 if __name__ == "__main__":
-    # log.debug(config)
-    log.info('start bot via main()')
-    log.info(bot)
 
     main()
