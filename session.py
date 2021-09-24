41246cc70cbeb0e967a1498afa843401c8710b20
And They Have a Plan <etconsilium@github.com>
2021-09-24 16:51:46 +0300

    local session add

---
+++ session.py
@@ -0,0 +1,18 @@
+####
+##
+#
+#
+from models import Record
+
+
+class Session(Record):
+    """
+    classdocs
+    """
+
+
+# def __init__(self, params):
+#     '''
+#     Constructor
+#     '''
+#     super().__init__()
